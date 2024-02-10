1b9c3ed551fdc9c174b0b8f15fabff9a07acbed9
tkdn <tkdnation@gmail.com>
2024-02-10 14:08:16 +0900

    feat: EC2ユーザデータの追加
    - https://docs.aws.amazon.com/ja_jp/AWSEC2/latest/UserGuide/user-data.html

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ resource "aws_instance" "example" {
   ami = "ami-067983a1f071c98a2"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  # 元のインスタンスをterminateして実行したいため
+  user_data_replace_on_change = true
+
   tags = {
     Name = "terraform-example"
   }
