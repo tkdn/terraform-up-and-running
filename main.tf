657370efd82eb8437e61d2289d0bdb477b8284e7
tkdn <tkdnation@gmail.com>
2024-02-10 14:10:28 +0900

    feat: EC2インスタンスがSGを利用するよう変更

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-067983a1f071c98a2"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.instance.id ]
 
   user_data = <<-EOF
               #!/bin/bash
