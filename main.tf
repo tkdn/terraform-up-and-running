88454517468f0a0f50fbbc986090e957f2ea3c34
tkdn <tkdnation@gmail.com>
2024-02-10 14:18:28 +0900

    chore: fmt

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-067983a1f071c98a2"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.instance.id ]
+  ami                    = "ami-067983a1f071c98a2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -24,9 +24,9 @@ resource "aws_security_group" "instance" {
   name = "terraforom-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
