a144d56170104a2622055b3ee96fe5f44aec9dfd
tkdn <tkdnation@gmail.com>
2024-02-12 20:09:42 +0900

    feat: use module outputs

--- ch4.modules/modules/webserver-cluster/outputs.tf
+++ ch4.modules/modules/webserver-cluster/outputs.tf
@@ -2,3 +2,8 @@ output "alb_dns_name" {
   value       = aws_alb.example.dns_name
   description = "The domain name of the LB"
 }
+
+output "asg_name" {
+  value       = aws_autoscaling_group.example.name
+  description = "The name of the ASG"
+}
