3f1bf7fd60b9e1c9e10fb8514427ab6c599d448d
tkdn <tkdnation@gmail.com>
2024-02-16 18:29:44 +0900

    feat: use modules

--- ch5.tips-and-tricks/live/global/iam/mian.tf
+++ ch5.tips-and-tricks/live/global/iam/mian.tf
@@ -2,9 +2,12 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_iam_user" "example" {
-  count = length(var.user_names)
-  name  = var.user_names[count.index]
+
+module "users" {
+  source = "../../../modules/landing-zone"
+
+  count     = length(var.user_names)
+  user_name = var.user_names[count.index]
 }
 
 variable "user_names" {
@@ -12,10 +15,6 @@ variable "user_names" {
   default = ["neo", "trinity", "morphes"]
 }
 
-output "all_arns" {
-  value = aws_iam_user.example[*].arn
-}
-
-output "first_user_arn" {
-  value = aws_iam_user.example[0].arn
+output "users_arn" {
+  value = module.users[*].user_arn
 }
