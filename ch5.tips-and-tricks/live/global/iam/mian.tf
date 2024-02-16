4c6e55490ca96f0815e94d2cda109e591efa5342
tkdn <tkdnation@gmail.com>
2024-02-16 18:23:19 +0900

    feat: ref values in index access

--- ch5.tips-and-tricks/live/global/iam/mian.tf
+++ ch5.tips-and-tricks/live/global/iam/mian.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_iam_user" "exmapl" {
+resource "aws_iam_user" "example" {
   count = length(var.user_names)
   name  = var.user_names[count.index]
 }
@@ -10,4 +10,12 @@ resource "aws_iam_user" "exmapl" {
 variable "user_names" {
   type    = list(string)
   default = ["neo", "trinity", "morphes"]
+}
+
+output "all_arns" {
+  value = aws_iam_user.example[*].arn
+}
+
+output "first_user_arn" {
+  value = aws_iam_user.example[0].arn
 }
