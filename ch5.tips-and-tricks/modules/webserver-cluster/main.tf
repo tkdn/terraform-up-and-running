9a179f20c8bb82741915ea53417b4959b2e4759f
tkdn <tkdnation@gmail.com>
2024-02-17 14:34:33 +0900

    fix: SG ingress/egress ルールの重複削除

--- ch5.tips-and-tricks/modules/webserver-cluster/main.tf
+++ ch5.tips-and-tricks/modules/webserver-cluster/main.tf
@@ -72,13 +72,16 @@ resource "aws_autoscaling_schedule" "scale_in_at_night" {
 
 resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
+}
 
-  ingress {
+resource "aws_security_group_rule" "name" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instance.id
+
     from_port   = var.server_port
     to_port     = var.server_port
     protocol    = local.tcp_protocol
     cidr_blocks = local.all_ips
-  }
 }
 
 resource "aws_alb" "example" {
@@ -122,19 +125,6 @@ resource "aws_lb_listener_rule" "asg" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
-
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
 }
 
 resource "aws_security_group_rule" "allow_http_inbound" {
