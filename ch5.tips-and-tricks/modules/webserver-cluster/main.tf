5cd9d80cdeea723bf72f87c0e53a4fe076ef5c42
tkdn <tkdnation@gmail.com>
2024-02-17 13:06:25 +0900

    feat: condition and for_each, for

--- ch5.tips-and-tricks/modules/webserver-cluster/main.tf
+++ ch5.tips-and-tricks/modules/webserver-cluster/main.tf
@@ -32,7 +32,11 @@ resource "aws_autoscaling_group" "example" {
   }
 
   dynamic "tag" {
-    for_each = var.custom_tags
+    for_each = {
+      for key, value in var.custom_tags :
+      key => upper(value)
+      if key != "Name"
+    }
 
     content {
       key                 = tag.key
