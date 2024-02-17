5f0efa8f8fc5dee53b1f2f619a6645e66392c052
tkdn <tkdnation@gmail.com>
2024-02-17 15:12:18 +0900

    feat: v2 デプロイ

--- ch5.tips-and-tricks/live/prod/services/webserver-cluster/main.tf
+++ ch5.tips-and-tricks/live/prod/services/webserver-cluster/main.tf
@@ -5,6 +5,9 @@ provider "aws" {
 module "webserver_cluster" {
   source = "../../../../modules/webserver-cluster"
 
+  ami         = "ami-067983a1f071c98a2"
+  server_text = "Hello, New World"
+
   cluster_name           = "webservers-prod"
   db_remote_state_bucket = "tkdn-terraform-up-and-running-state"
   db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
