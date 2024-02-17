ec76bd66c84811af322a9a3f6892fb54d235526c
tkdn <tkdnation@gmail.com>
2024-02-17 13:20:35 +0900

    feat: if and for directives

--- ch5.tips-and-tricks/live/for-loop/main.tf
+++ ch5.tips-and-tricks/live/for-loop/main.tf
@@ -23,3 +23,11 @@ output "uppper_roles" {
 output "for_directive" {
   value = "%{for i, name in var.names}(${i})-${name}, %{endfor}"
 }
+
+output "for_directive_index_if" {
+  value = <<EOF
+%{~for i, name in var.names~}
+${name}%{if i < length(var.names) - 1}, %{else}.%{endif}
+%{~endfor~}
+EOF
+}
