4c388fdbbf4dbad71c56b18ca115754008aaa804
tkdn <tkdnation@gmail.com>
2024-02-24 10:28:55 +0900

    feat: 複数リージョンに同じAMIのEC2インスタンスを起動する

--- ch7.multiple-providers/multi-regions/main.tf
+++ ch7.multiple-providers/multi-regions/main.tf
@@ -1,15 +1,45 @@
-data "aws_region" "region_1" {
+resource "aws_instance" "region_1" {
   provider = aws.region_1
+
+  ami           = data.aws_ami.ubuntu_region_1.id
+  instance_type = "t2.micro"
 }
 
-data "aws_region" "region_2" {
+resource "aws_instance" "region_2" {
   provider = aws.region_2
+
+  ami           = data.aws_ami.ubuntu_region_2.id
+  instance_type = "t2.micro"
 }
 
-output "region_1" {
-  value = data.aws_region.region_1.name
+data "aws_ami" "ubuntu_region_1" {
+  provider = aws.region_1
+
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 }
 
-output "region_2" {
-  value = data.aws_region.region_2.name
+data "aws_ami" "ubuntu_region_2" {
+  provider = aws.region_2
+
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+output "region_1_instance_az" {
+  value = aws_instance.region_1.availability_zone
+}
+
+output "region_2_instance_az" {
+  value = aws_instance.region_2.availability_zone
 }
