2a480c4587465b1d70a2b3a20a4f10114d402ea9
tkdn <tkdnation@gmail.com>
2024-03-03 17:09:35 +0900

    feat: ALBをモジュール化

--- ch8.production-grade-infrastructure/modules/webserver-cluster/main.tf
+++ ch8.production-grade-infrastructure/modules/webserver-cluster/main.tf
@@ -9,29 +9,6 @@ terraform {
   }
 }
 
-resource "aws_alb" "example" {
-  name               = var.cluster_name
-  load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb.id]
-}
-
-resource "aws_alb_listener" "http" {
-  load_balancer_arn = aws_alb.example.arn
-  port              = local.http_port
-  protocol          = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-
 resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_alb_listener.http.arn
   priority     = 100
@@ -46,30 +23,6 @@ resource "aws_lb_listener_rule" "asg" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.asg.arn
   }
-}
-
-resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
-}
-
-resource "aws_security_group_rule" "allow_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = local.http_port
-  to_port     = local.http_port
-  protocol    = local.tcp_protocol
-  cidr_blocks = local.all_ips
-}
-
-resource "aws_security_group_rule" "allow_all_outbound" {
-  type              = "egress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = local.any_port
-  to_port     = local.any_port
-  protocol    = local.any_protocol
-  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb_target_group" "asg" {
@@ -108,12 +61,4 @@ data "terraform_remote_state" "db" {
     key    = var.db_remote_state_key
     region = "ap-northeast-1"
   }
-}
-
-locals {
-  http_port    = 80
-  any_port     = 0
-  any_protocol = "-1"
-  tcp_protocol = "tcp"
-  all_ips      = ["0.0.0.0/0"]
 }
