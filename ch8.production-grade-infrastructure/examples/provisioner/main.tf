edc15cea40324f2a42cfab6408aa9f0fd0ae323c
tkdn <tkdnation@gmail.com>
2024-03-09 15:38:15 +0900

    feat: provisioner remote ssh

--- ch8.production-grade-infrastructure/examples/provisioner/main.tf
+++ ch8.production-grade-infrastructure/examples/provisioner/main.tf
@@ -13,11 +13,54 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+resource "aws_security_group" "instance" {
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    # 現実では信頼できるIPやサブネットマスクを指定し接続を許可、ここでは簡易的すぎる
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 現実ではTerraformと別でSSH鍵を管理すべきだが、簡易的にプライベートキーを作成
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  public_key = tls_private_key.example.public_key_openssh
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-019d9da0f68cbf7c0"
-  instance_type = "t2.micro"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   provisioner "local-exec" {
     command = "echo \"hello, world  from $(uname -smp)\""
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo \"hello, world  from $(uname -smp)\""]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = tls_private_key.example.private_key_pem
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
