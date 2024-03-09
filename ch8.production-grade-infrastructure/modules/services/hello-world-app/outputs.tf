c0287768cf380c548cd46f0b25e73c27573887a5
tkdn <tkdnation@gmail.com>
2024-03-09 12:11:23 +0900

    feat: hello-world-app モジュールで ASG, ALB モジュールを使う

--- ch8.production-grade-infrastructure/modules/services/hello-world-app/outputs.tf
+++ ch8.production-grade-infrastructure/modules/services/hello-world-app/outputs.tf
@@ -1,14 +1,14 @@
 output "alb_dns_name" {
-  value       = aws_alb.example.dns_name
+  value       = module.alb.alb_dns_name
   description = "The domain name of the LB"
 }
 
 output "asg_name" {
-  value       = aws_autoscaling_group.example.name
+  value       = module.asg.asg_name
   description = "The name of the ASG"
 }
 
 output "alb_security_group_id" {
-  value       = aws_security_group.alb.id
+  value       = module.asg.instance_security_group_id
   description = "The ID of the SG attached to the ALB"
 }
--- ch8.production-grade-infrastructure/modules/services/hello-world-app/variables.tf
+++ ch8.production-grade-infrastructure/modules/services/hello-world-app/variables.tf
@@ -18,3 +18,44 @@ variable "db_remote_state_key" {
   description = "The path for database's remote state in S3"
   type        = string
 }
+
+variable "environment" {
+  description = "environment, deployed to; e.g. production, stating"
+  type        = string
+}
+
+
+/**
+ * variables for module alb
+ */
+variable "ami" {
+  description = "The AMI to run in the cluster"
+  type        = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "server_text" {
+  description = "The text the web server should return"
+  type        = string
+  default     = "Hello, World"
+}
+
+variable "min_size" {
+  type = number
+}
+
+variable "max_size" {
+  type = number
+}
+
+variable "enable_autoscaling" {
+  type = bool
+}
+
+variable "custom_tags" {
+  type    = map(string)
+  default = {}
+}
