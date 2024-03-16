f8a5142e78d13d392f2206c8da565e14dd574966
tkdn <tkdnation@gmail.com>
2024-03-16 13:41:28 +0900

    feat: Hello World App モジュールがパラメータにより VPC ID, サブネットID, MySQL有無や設定のパラメータを注入できるようリファクタリング

--- ch9.testing/modules/services/hello-world-app/dependencies.tf
+++ ch9.testing/modules/services/hello-world-app/dependencies.tf
@@ -1,4 +1,6 @@
 data "terraform_remote_state" "db" {
+  count = var.mysql_config == null ? 1 : 0
+
   backend = "s3"
 
   config = {
@@ -9,12 +11,32 @@ data "terraform_remote_state" "db" {
 }
 
 data "aws_vpc" "default" {
+  count   = var.vpc_id == null ? 1 : 0
   default = true
 }
 
 data "aws_subnets" "default" {
+  count = var.subnet_ids == null ? 1 : 0
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+    values = [local.vpc_id]
   }
+}
+
+locals {
+  mysql_config = (
+    var.mysql_config == null
+    ? data.terraform_remote_state.db[0].outputs
+    : var.mysql_config
+  )
+  vpc_id = (
+    var.vpc_id == null
+    ? data.aws_vpc.default[0].id
+    : var.vpc_id
+  )
+  subnet_ids = (
+    var.subnet_ids == null
+    ? data.aws_subnets.default[0].ids
+    : var.subnet_ids
+  )
 }
--- ch9.testing/modules/services/hello-world-app/main.tf
+++ ch9.testing/modules/services/hello-world-app/main.tf
@@ -18,15 +18,15 @@ module "asg" {
 
   user_data = templatefile("${path.module}/user-data.sh", {
     server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
+    db_address  = local.mysql_config.adress
+    db_port     = local.mysql_config.port
     server_text = var.server_text
   })
 
   min_size           = var.min_size
   max_size           = var.max_size
   enable_autoscaling = var.enable_autoscaling
-  subnet_ids         = data.aws_subnets.default.ids
+  subnet_ids         = local.subnet_ids
   target_group_arns  = [aws_lb_target_group.asg.arn]
   health_check_type  = "ELB"
 
@@ -37,7 +37,7 @@ module "alb" {
   source = "../../networking/alb"
 
   alb_name   = "hello-world-${var.environment}"
-  subnet_ids = data.aws_subnets.default.ids
+  subnet_ids = local.subnet_ids
 }
 
 resource "aws_lb_listener_rule" "asg" {
@@ -60,7 +60,7 @@ resource "aws_lb_target_group" "asg" {
   name     = "hello-world-${var.environment}"
   port     = var.server_port
   protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
+  vpc_id   = local.vpc_id
 
   health_check {
     path                = "/"
