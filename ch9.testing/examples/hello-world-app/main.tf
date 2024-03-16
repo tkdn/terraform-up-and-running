ac9aa4eb76b9e4733c4d17aae50b7fc507337bad
tkdn <tkdnation@gmail.com>
2024-03-16 13:18:44 +0900

    feat: Hello World App モジュールで mysql モジュールを利用し db address, port を渡す
    - 書籍では関数の合成と表現している

--- ch9.testing/examples/hello-world-app/main.tf
+++ ch9.testing/examples/hello-world-app/main.tf
@@ -8,14 +8,21 @@ module "hello_world_app" {
   server_text = "Hello, World"
   environment = "example"
 
-  db_remote_state_bucket = "(BUCKET_NAME)"
-  db_remote_state_key    = "examples/terraform.tfstate"
+  mysql_config = module.mysql
 
   instance_type      = "t2.micro"
   min_size           = 2
   max_size           = 2
   enable_autoscaling = false
   ami                = data.aws_ami.ubuntu.id
+}
+
+module "mysql" {
+  source = "../../modules/data-stores/mysql"
+
+  db_name     = var.db_name
+  db_username = var.db_username
+  db_password = var.db_password
 }
 
 data "aws_ami" "ubuntu" {
--- ch9.testing/modules/services/hello-world-app/variables.tf
+++ ch9.testing/modules/services/hello-world-app/variables.tf
@@ -7,11 +7,13 @@ variable "server_port" {
 variable "db_remote_state_bucket" {
   description = "The name of the S3 bucket for the database's remote state"
   type        = string
+  default     = null
 }
 
 variable "db_remote_state_key" {
   description = "The path for database's remote state in S3"
   type        = string
+  default     = null
 }
 
 variable "environment" {
@@ -53,4 +55,26 @@ variable "enable_autoscaling" {
 variable "custom_tags" {
   type    = map(string)
   default = {}
+}
+
+/**
+ * 新しく追加する変数
+*/
+
+variable "vpc_id" {
+  type    = string
+  default = null
+}
+
+variable "subnet_ids" {
+  type    = list(string)
+  default = null
+}
+
+variable "mysql_config" {
+  type = object({
+    address = string
+    port    = number
+  })
+  default = null
 }
