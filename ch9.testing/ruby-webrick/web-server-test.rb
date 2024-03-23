49dbab19705058cf623dc0ad26dd2b8ac7ac343c
tkdn <tkdnation@gmail.com>
2024-03-23 10:30:09 +0900

    feat: Ruby Webサーバ統合テスト

--- ch9.testing/ruby-webrick/web-server-test.rb
+++ ch9.testing/ruby-webrick/web-server-test.rb
@@ -1,5 +1,6 @@
 require_relative './web-server'
 require "test/unit"
+require 'net/http'
 
 class TestWebServer < Test::Unit::TestCase
   def initialize(test_method)
@@ -26,5 +27,47 @@ class TestWebServer < Test::Unit::TestCase
     assert_equal(404, s)
     assert_equal('text/plain', ct)
     assert_equal('Not Found', b)
+  end
+
+  def test_integration_hello
+    do_integration_test('/', lambda {|response|
+      assert_equal(200, response.code.to_i)
+      assert_equal('text/plain', response['Content-Type'])
+      assert_equal('Hello, World', response.body)
+    })
+  end
+
+  def test_integration_api
+    do_integration_test('/api', lambda {|response|
+      assert_equal(201, response.code.to_i)
+      assert_equal('application/json', response['Content-Type'])
+      assert_equal('{"foo": 1}', response.body)
+    })
+  end
+
+  def test_integration_404
+    do_integration_test('/not-found', lambda {|response|
+      assert_equal(404, response.code.to_i)
+      assert_equal('text/plain', response['Content-Type'])
+      assert_equal('Not Found', response.body)
+    })
+  end
+
+  def do_integration_test(path, check_response)
+    port = 8000
+    server = WEBrick::HTTPServer.new(Port: port)
+    server.mount '/', WebServer
+
+    begin
+      thread = Thread.new do
+        server.start
+      end
+      uri = URI("http://localhost:#{port}#{path}")
+      response = Net::HTTP.get_response(uri)
+      check_response.call(response)
+    ensure
+      server.shutdown
+      thread.join
+    end
   end
 end
