caaad6cbd43b10c9862c8aecba18cc9bb91b73d2
tkdn <tkdnation@gmail.com>
2024-03-23 13:15:13 +0900

    chore: InvalidParameterCombination が出るので t3.micro に変更した

--- ch9.testing/modules/data-stores/mysql/main.tf
+++ ch9.testing/modules/data-stores/mysql/main.tf
@@ -13,7 +13,7 @@ resource "aws_db_instance" "example" {
   identifier_prefix   = "terraform-up-and-running"
   engine              = "mysql"
   allocated_storage   = 10
-  instance_class      = "db.t2.micro"
+  instance_class      = "db.t3.micro"
   db_name             = var.db_name
   username            = var.db_username
   password            = var.db_password
